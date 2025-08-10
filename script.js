0852d28ac4b226ced09920851a755405cf01fa24
Ruben Lazell <rubenlazell7@aol.com>
2025-08-10 00:14:40 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Selects the button element by its ID
+document.getElementById("myButton").addEventListener("click", function() {
+    // Runs when the button is clicked
+    alert("You clicked the button!"); // Displays a popup message
+});
